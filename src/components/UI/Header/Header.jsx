1739540ab23d2d9de817faec4f2de8c0c2bc6152
kobasisauri <kobasisauri@gmail.com>
2022-10-29 15:55:32 +0400

    feat: header

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -1,15 +1,46 @@
 import styled from "./Header.module.scss";
-// import Logo from "../../../assets/logo-name.svg";
 import { ReactComponent as Logo } from "../../../assets/Logo.svg";
+import { ReactComponent as SearchIcon } from "../../../assets/search.svg";
+import { useState } from "react";
+import { act } from "react-dom/test-utils";
+
+const tabEnam = {
+  home: "Home",
+  about: "About",
+  shop: "Shop",
+};
+
+const tabArr = ["Home", "About", "Shop"];
 
 const Header = () => {
+  const [active, setActive] = useState(tabEnam.home);
+
   return (
     <div className={styled.header}>
       <div>
         <Logo className={styled.logo} />
       </div>
-      <div></div>
-      <div></div>
+      <div className={styled.items}>
+        {tabArr.map((item, i) => (
+          <div
+            key={i}
+            onClick={() => setActive(item)}
+            className={item === active ? styled.active : ""}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className={styled.search}>
+        <div>
+          <SearchIcon className={styled["search-icon"]} />
+        </div>
+        <div className={styled["burger-wrapper"]}>
+          <div className={styled.burger} />
+          <div className={styled.burger} />
+          <div className={styled.burger} />
+        </div>
+      </div>
     </div>
   );
 };
